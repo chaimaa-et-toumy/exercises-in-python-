8fbf84385acb0ff5043482586197a1300f435973
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-07-05 10:25:44 +0100

    a mad libs game

---
+++ mad-libs.py
@@ -0,0 +1,7 @@
+color = input("enter color : ")
+plural_noun = input("enter a plural noun : ")
+adjective = input("enter an adjective : ")
+
+print("three are " + color)
+print(plural_noun + " people are mean")
+print("please keep it " + adjective)
