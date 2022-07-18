a01e822290aa3821cd49ae58289a3a688a1b3bf4
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-07-18 14:09:20 +0100

    Classes && Object

--- Classes-Objects.py
+++ Classes-Objects.py
@@ -5,8 +5,14 @@ class Employee:
         self.cin = cin
         self.age = age
         self.salary = salary
+    
+    def myfunc(self):
+        print("Hello my name is " + self.FirstName)
+
 # Objet
 Emp1 = Employee("mohamed" , "el-meghari" , "AB1343" , 21 , "3000")
 Emp2 = Employee("rida" , "taoussi" , "AB2243" , 25 , "6000")
+
+Emp1.myfunc() # Hello my name is mohamed
 
 print(Emp1.FirstName , Emp1.lastName)
