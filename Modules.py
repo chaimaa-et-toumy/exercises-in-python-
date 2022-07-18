1278644d71be769016a34f335b169943993b3880
chaimaa et-toumy <chaimaetoumy5@gmail.com>
2022-07-18 13:26:49 +0100

    Modules

---
+++ Modules.py
@@ -0,0 +1,8 @@
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+def greeting(name):
+  print("Hello, " + name)
---
+++ test-modules.py
@@ -0,0 +1,6 @@
+import Modules
+
+Modules.greeting("Jonathan")
+
+a = Modules.person1["age"]
+print(a)
